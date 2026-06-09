83975eff2d479fb29369687f46f3f5996307c195
Amir-017 <amiirahmed017@gmail.com>
2026-06-09 22:53:34 +0300

    put real origin cors to make project working in global

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(cors(
-  {origin: "http://localhost:5173",
+  {origin: "https://market-project-connected-with-back-6pzx7jap0.vercel.app",
   credentials: true,
 }
 ))
