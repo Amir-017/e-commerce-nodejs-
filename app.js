d64c9324fb419e27c55f3aa1bc03f0eb7ae41d81
Amir-017 <amiirahmed017@gmail.com>
2026-06-10 18:46:26 +0300

    enchance

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(cors(
-  {origin: "https://market-project-connected-with-back.vercel.app",
+  {origin: "http://localhost:5173",
   credentials: true,
 }
 ))
