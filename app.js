cabb545d80094299bad198b54c3e7a330c783964
Amir-017 <amiirahmed017@gmail.com>
2026-06-16 21:50:56 +0300

    make some updates and add new feature

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
- // http://localhost:5173
+ // https://market-project-connected-with-back.vercel.app
 
 app.use(cors(
-  {origin: "https://market-project-connected-with-back.vercel.app",
+  {origin: "http://localhost:5173",
   credentials: true,
 }
 ))
